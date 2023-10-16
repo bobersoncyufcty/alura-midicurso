07e7bb28251543049e46dadeb33a7e0dc74df93c
bobersoncyufcty <141032297+bobersoncyufcty@users.noreply.github.com>
2023-10-16 09:00:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Pom ---------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+//enquanto
+while (contador < 9) {
+    
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
